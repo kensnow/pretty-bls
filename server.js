108479f0e6cb00dc48f1c7e43be785b45003e014
Ken Snow <k.snowjr@gmail.com>
2019-06-17 12:24:17 -0600

    deploy ready

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const expressJwt = require('express-jwt')
 require('dotenv').config()
 
 const app = express()
+const path = require('path')
 
 app.use(express.json())
 app.use(morgan('dev'))
@@ -14,16 +15,24 @@ app.use('/auth', require('./routes/auth'))
 app.use('/api/profile', require('./routes/profile'))
 app.use('/api/bls', require('./routes/bls'))
 app.use('/study', require('./routes/study'))
+
+app.use(express.static(path.join(__dirname, 'client', 'build')))
+
 app.use((err, req, res, next) => {
-    console.error(err)
     if (err.name === 'UnauthorizedError'){
         res.status(err.status)
     }
     return res.send({message: err.message})
 })
 
+
+
 mongoose.connect(process.env.MONGODB_URI, () => {
     console.log('connected to MANGODB')
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 })
 
 app.listen(process.env.PORT, () => {
